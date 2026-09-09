ef11ea9b9631e4280aa2ab1cb1fd23492ae75324
Rômulo Gomes <romulocuias@gmail.com>
2026-09-09 09:27:43 -0300

    Version 1.0

---
+++ main.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+
+
+janela = ctk.CTk()
+janela.title("Sistema Teste")
+janela.geometry("400x200")
+button = ctk.CTkButton(janela, text="Clique aqui", command=LookupError)
+button.pack(pady=20)
+mensagem = ctk.CTkLabel(janela, text="Ben vindo ao sitema Teste")
+mensagem.pack(expand=True)
+
+janela.mainloop()
